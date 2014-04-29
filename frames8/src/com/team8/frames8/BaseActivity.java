f0cc26f63744e38323408a43a29efd4ce742a750
Raakesh Dhanasekaran <raakeshrds@gmail.com>
2014-04-29 18:02:03 -0500

    picture

---
+++ frames8/src/com/team8/frames8/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.team8.frames8;
+
+import android.app.Activity;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+/**
+ * @author Paresh Mayani (@pareshmayani)
+ */
+public abstract class BaseActivity extends Activity {
+
+ protected ImageLoader imageLoader = ImageLoader.getInstance();
+
+}
